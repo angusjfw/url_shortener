577e4264b68918775378394602f85cc29f39291d
hibreez <4ngus.whitehead@gmail.com>
2016-02-22 18:07:49 +0000

    Modify test and create helpers.

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ So that I can visit my site
 I want my shortened URL to redirect to the longer original
 
 As a user
-So that I don't link people to the wrong address
-I want to only be able to enter a valid URL
-
-As a user
 So that I can keep using my shortened URL
 I want it to be persisted as a link to the longer version
+
+As a user
+So that I don't link people to the wrong address
+I want to only be able to enter a valid URL
 
 As a administrator
 So that my database remains clean
--- spec/features/1_shorten_URL_spec.rb
+++ spec/features/1_shorten_URL_spec.rb
@@ -6,7 +6,7 @@ feature 'Providing short URls' do
   scenario 'when a URL is entered in the form' do
     allow(SecureRandom).to receive(:urlsafe_base64).and_return('random')
     visit '/links/new'
-    fill_in 'url', with: 'https://www.google.co.uk/#q=i+need+a+url+shortener'
+    fill_in 'url', with: 'https://www.google.co.uk/?q=i+need+a+url+shortener'
     click_button 'SHORTEN'
 
     expect(page.current_path).to include('/links/random')
@@ -16,7 +16,7 @@ feature 'Providing short URls' do
   scenario 'when a different URL is entered in the form' do
     allow(SecureRandom).to receive(:urlsafe_base64).and_return('different')
     visit '/links/new'
-    fill_in 'url', with: 'https://www.google.co.uk/#q=this+is+different'
+    fill_in 'url', with: 'https://www.google.co.uk/?q=this+is+different'
     click_button 'SHORTEN'
 
     expect(page.current_path).to include('/links/different')
