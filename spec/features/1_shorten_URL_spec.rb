be6178ade6783340976e0f5bb3f064922b823f3c
hibreez <4ngus.whitehead@gmail.com>
2016-02-22 16:19:43 +0000

    Create unit test for method to generate codes with secure random.

--- spec/features/1_shorten_URL_spec.rb
+++ spec/features/1_shorten_URL_spec.rb
@@ -4,7 +4,7 @@
 
 feature 'Providing short URls' do
   scenario 'when a URL is entered in the form' do
-    allow(SecureRandom).to receive(:base64).and_return('random')
+    allow(SecureRandom).to receive(:urlsafe_base64).and_return('random')
     visit '/links/new'
     fill_in 'url', with: 'https://www.google.co.uk/#q=i+need+a+url+shortener'
     click_button 'SHORTEN'
@@ -14,7 +14,7 @@ feature 'Providing short URls' do
   end
 
   scenario 'when a different URL is entered in the form' do
-    allow(SecureRandom).to receive(:base64).and_return('different')
+    allow(SecureRandom).to receive(:urlsafe_base64).and_return('different')
     visit '/links/new'
     fill_in 'url', with: 'https://www.google.co.uk/#q=this+is+different'
     click_button 'SHORTEN'
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,7 +1,15 @@
 describe Link do
 
-  xdescribe '' do
-    xit '' do
+  let!(:link) do
+    Link.new(original_url: 'https://www.google.co.uk/#q=i+need+a+url+shortener')
+  end
+
+  describe '#generate_code' do
+    it 'generates random codes using secure random' do
+      allow(SecureRandom).to receive(:urlsafe_base64).and_return('soy_random')
+      link.generate_code
+
+      expect(link.code).to eq 'soy_random'
     end
   end
 end
